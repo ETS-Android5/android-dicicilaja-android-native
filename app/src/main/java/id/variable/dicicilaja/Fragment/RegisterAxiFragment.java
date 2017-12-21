92300ed55e20a2d267bfc2a5c2b8c8902a0e8b9d
Ziady Mubaraq <ziady.mubaraq@gmail.com>
2017-12-21 23:26:48 +0700

    Update Register Customer, Register Axi

--- app/src/main/java/id/variable/dicicilaja/Fragment/RegisterAxiFragment.java
+++ app/src/main/java/id/variable/dicicilaja/Fragment/RegisterAxiFragment.java
@@ -1,12 +1,21 @@
 package id.variable.dicicilaja.Fragment;
 
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatSpinner;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import id.variable.dicicilaja.Activity.HelpActivity;
+import id.variable.dicicilaja.Activity.LoginActivity;
+import id.variable.dicicilaja.Activity.RegisterAxi2Activity;
 import id.variable.dicicilaja.R;
 
 /**
@@ -14,7 +23,10 @@ import id.variable.dicicilaja.R;
  */
 public class RegisterAxiFragment extends Fragment {
 
-
+    TextView titleSection, bodySection, detailSection, sudahPunyaAkun, judulSudahPunyaAkun;
+    AppCompatSpinner selectTempat, selectTempat2;
+    EditText inputReferal, inputNama, inputEmail, inputHandphone, inputIbu;
+    Button btnLanjut;
     public RegisterAxiFragment() {
         // Required empty public constructor
     }
@@ -25,6 +37,47 @@ public class RegisterAxiFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_register_axi, container, false);
+        inputNama = (EditText) view.findViewById(R.id.inputNama);
+        inputEmail = (EditText) view.findViewById(R.id.inputEmail);
+        inputIbu = (EditText) view.findViewById(R.id.inputIbu);
+        inputReferal = (EditText) view.findViewById(R.id.inputReferal);
+        inputHandphone = (EditText) view.findViewById(R.id.inputHandphone);
+        sudahPunyaAkun = (TextView) view.findViewById(R.id.sudahPunyaAkun);
+        judulSudahPunyaAkun = (TextView) view.findViewById(R.id.judulSudahPunyaAkun);
+        titleSection = (TextView) view.findViewById(R.id.titleSection);
+        bodySection = (TextView) view.findViewById(R.id.bodySection);
+        detailSection = (TextView) view.findViewById(R.id.detailSection);
+        btnLanjut = (Button) view.findViewById(R.id.btnLanjut);
+        selectTempat = (AppCompatSpinner) view.findViewById(R.id.selectTempat);
+        selectTempat2 = (AppCompatSpinner) view.findViewById(R.id.selectTempat2);
+        Typeface opensans_extrabold = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-ExtraBold.ttf");
+        Typeface opensans_bold = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-Bold.ttf");
+        Typeface opensans_semibold = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-SemiBold.ttf");
+        Typeface opensans_reguler = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-Regular.ttf");
+        inputNama.setTypeface(opensans_semibold);
+        inputEmail.setTypeface(opensans_semibold);
+        inputHandphone.setTypeface(opensans_semibold);
+        inputIbu.setTypeface(opensans_semibold);
+        inputReferal.setTypeface(opensans_semibold);
+        judulSudahPunyaAkun.setTypeface(opensans_reguler);
+        sudahPunyaAkun.setTypeface(opensans_semibold);
+        titleSection.setTypeface(opensans_bold);
+        bodySection.setTypeface(opensans_reguler);
+        detailSection.setTypeface(opensans_semibold);
+        btnLanjut.setTypeface(opensans_bold);
+        sudahPunyaAkun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().finish();
+            }
+        });
+        btnLanjut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), RegisterAxi2Activity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 
--- app/src/main/java/id/variable/dicicilaja/Fragment/RegisterCustomerFragment.java
+++ app/src/main/java/id/variable/dicicilaja/Fragment/RegisterCustomerFragment.java
@@ -1,12 +1,19 @@
 package id.variable.dicicilaja.Fragment;
 
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import id.variable.dicicilaja.Activity.LoginActivity;
+import id.variable.dicicilaja.Activity.RegisterActivity;
 import id.variable.dicicilaja.R;
 
 /**
@@ -14,7 +21,9 @@ import id.variable.dicicilaja.R;
  */
 public class RegisterCustomerFragment extends Fragment {
 
-
+    EditText inputNama, inputEmail, inputHandphone, inputKataSandi, inputKonfirmasi;
+    TextView textCheck, judulSudahPunyaAkun, sudahPunyaAkun, titleSection, bodySection, detailSection;
+    Button btnDaftar;
     public RegisterCustomerFragment() {
         // Required empty public constructor
     }
@@ -25,7 +34,40 @@ public class RegisterCustomerFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_register_customer, container, false);
-
+        inputNama = (EditText) view.findViewById(R.id.inputNama);
+        inputEmail = (EditText) view.findViewById(R.id.inputEmail);
+        inputHandphone = (EditText) view.findViewById(R.id.inputHandphone);
+        inputKataSandi = (EditText) view.findViewById(R.id.inputKataSandi);
+        inputKonfirmasi = (EditText) view.findViewById(R.id.inputKonfirmasi);
+        textCheck = (TextView) view.findViewById(R.id.textCheck);
+        judulSudahPunyaAkun = (TextView) view.findViewById(R.id.judulSudahPunyaAkun);
+        sudahPunyaAkun = (TextView) view.findViewById(R.id.sudahPunyaAkun);
+        titleSection = (TextView) view.findViewById(R.id.titleSection);
+        bodySection = (TextView) view.findViewById(R.id.bodySection);
+        detailSection = (TextView) view.findViewById(R.id.detailSection);
+        btnDaftar = (Button) view.findViewById(R.id.btnDaftar);
+        Typeface opensans_extrabold = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-ExtraBold.ttf");
+        Typeface opensans_bold = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-Bold.ttf");
+        Typeface opensans_semibold = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-SemiBold.ttf");
+        Typeface opensans_reguler = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-Regular.ttf");
+        inputNama.setTypeface(opensans_semibold);
+        inputEmail.setTypeface(opensans_semibold);
+        inputHandphone.setTypeface(opensans_semibold);
+        inputKataSandi.setTypeface(opensans_semibold);
+        inputKonfirmasi.setTypeface(opensans_semibold);
+        textCheck.setTypeface(opensans_reguler);
+        judulSudahPunyaAkun.setTypeface(opensans_reguler);
+        sudahPunyaAkun.setTypeface(opensans_semibold);
+        titleSection.setTypeface(opensans_bold);
+        bodySection.setTypeface(opensans_reguler);
+        detailSection.setTypeface(opensans_semibold);
+        btnDaftar.setTypeface(opensans_bold);
+        sudahPunyaAkun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().finish();
+            }
+        });
         return view;
     }
 
