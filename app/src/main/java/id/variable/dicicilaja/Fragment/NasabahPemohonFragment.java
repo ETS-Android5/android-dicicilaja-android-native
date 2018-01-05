4a71106b220ab2308f02c3dd38fc4b00f44f8f89
Ziady Mubaraq <ziady.mubaraq@gmail.com>
2018-01-05 22:45:07 +0700

    Done Layouting Detail Pengajuan, (Pengajuan & Jaminan, Nasabah & Pemohon)

--- app/src/main/java/id/variable/dicicilaja/Fragment/NasabahPemohonFragment.java
+++ app/src/main/java/id/variable/dicicilaja/Fragment/NasabahPemohonFragment.java
@@ -1,11 +1,13 @@
 package id.variable.dicicilaja.Fragment;
 
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import id.variable.dicicilaja.R;
 
@@ -24,7 +26,19 @@ public class NasabahPemohonFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nasabah_pemohon, container, false);
+        View view =  inflater.inflate(R.layout.fragment_nasabah_pemohon, container, false);
+
+        TextView title_nasabah = view.findViewById(R.id.title_nasabah);
+        TextView title_pemohon = view.findViewById(R.id.title_pemohon);
+        Typeface opensans_extrabold = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-ExtraBold.ttf");
+        Typeface opensans_bold = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-Bold.ttf");
+        Typeface opensans_semibold = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-SemiBold.ttf");
+        Typeface opensans_reguler = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-Regular.ttf");
+
+        title_nasabah.setTypeface(opensans_bold);
+        title_pemohon.setTypeface(opensans_bold);
+
+        return view;
     }
 
 }
