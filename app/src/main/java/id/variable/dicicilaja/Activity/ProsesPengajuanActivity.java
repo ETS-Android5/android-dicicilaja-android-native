db3b8d92f5d753da6abb42c76a1cd6222341e4b2
Ziady Mubaraq <ziady.mubaraq@gmail.com>
2018-01-07 21:30:22 +0700

    Add AlertDialog on Proses Pengajuan TC

--- app/src/main/java/id/variable/dicicilaja/Activity/ProsesPengajuanActivity.java
+++ app/src/main/java/id/variable/dicicilaja/Activity/ProsesPengajuanActivity.java
@@ -1,12 +1,17 @@
 package id.variable.dicicilaja.Activity;
 
+import android.content.DialogInterface;
 import android.graphics.Typeface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import id.variable.dicicilaja.R;
 
@@ -27,10 +32,40 @@ public class ProsesPengajuanActivity extends AppCompatActivity {
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             window.setStatusBarColor(this.getResources().getColor(R.color.colorAccentDark));
         }
+        TextView konfirmasi = findViewById(R.id.konfirmasi);
+
+
+
         Typeface opensans_extrabold = Typeface.createFromAsset(getBaseContext().getAssets(), "fonts/OpenSans-ExtraBold.ttf");
         Typeface opensans_bold = Typeface.createFromAsset(getBaseContext().getAssets(), "fonts/OpenSans-Bold.ttf");
         Typeface opensans_semibold = Typeface.createFromAsset(getBaseContext().getAssets(), "fonts/OpenSans-SemiBold.ttf");
         Typeface opensans_reguler = Typeface.createFromAsset(getBaseContext().getAssets(), "fonts/OpenSans-Regular.ttf");
+
+        konfirmasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(ProsesPengajuanActivity.this);
+                // Setting Dialog Title
+                alertDialog.setTitle("KONFIRMASI");
+
+                // Setting Dialog Message
+                alertDialog.setMessage("Apakah Anda sudah menghubungi pemohon dan mengkonfirmasi bahwa benar adanya pengajuan tersebut.");
+
+                // Setting Icon to Dialog
+//                alertDialog.setIcon(R.drawable.ic_circle);
+
+                // Setting OK Button
+                alertDialog.setPositiveButton("Ya, saya sudah konfirmasi data", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Write your code here to execute after dialog closed
+                        Toast.makeText(getApplicationContext(), "You clicked on OK", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                // Showing Alert Message
+                alertDialog.show();
+            }
+        });
     }
 
     @Override
