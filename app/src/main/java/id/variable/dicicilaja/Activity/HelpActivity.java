2a2347eee81db645acdff3e8416474e0df87c093
Ziady Mubaraq <ziady.mubaraq@gmail.com>
2018-01-10 10:22:40 +0700

    Edit Notification Screen, Add ScrollView on Pengajuan Masuk Screen

--- app/src/main/java/id/variable/dicicilaja/Activity/HelpActivity.java
+++ app/src/main/java/id/variable/dicicilaja/Activity/HelpActivity.java
@@ -1,11 +1,13 @@
 package id.variable.dicicilaja.Activity;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import id.variable.dicicilaja.R;
 
@@ -24,6 +26,18 @@ public class HelpActivity extends AppCompatActivity {
 //            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 //            window.setStatusBarColor(this.getResources().getColor(R.color.colorBlack));
 //        }
+        TextView titleSection = findViewById(R.id.titleSection);
+        TextView bodySection = findViewById(R.id.bodySection);
+        TextView detailSection = findViewById(R.id.detailSection);
+
+        Typeface opensans_extrabold = Typeface.createFromAsset(getBaseContext().getAssets(), "fonts/OpenSans-ExtraBold.ttf");
+        Typeface opensans_bold = Typeface.createFromAsset(getBaseContext().getAssets(), "fonts/OpenSans-Bold.ttf");
+        Typeface opensans_semibold = Typeface.createFromAsset(getBaseContext().getAssets(), "fonts/OpenSans-SemiBold.ttf");
+        Typeface opensans_reguler = Typeface.createFromAsset(getBaseContext().getAssets(), "fonts/OpenSans-Regular.ttf");
+
+        titleSection.setTypeface(opensans_bold);
+        bodySection.setTypeface(opensans_reguler);
+        detailSection.setTypeface(opensans_semibold);
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
--- app/src/main/java/id/variable/dicicilaja/Fragment/RegisterMaxiFragment.java
+++ app/src/main/java/id/variable/dicicilaja/Fragment/RegisterMaxiFragment.java
@@ -1,11 +1,13 @@
 package id.variable.dicicilaja.Fragment;
 
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import id.variable.dicicilaja.R;
 
@@ -25,6 +27,20 @@ public class RegisterMaxiFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_register_maxi, container, false);
+
+        TextView titleSection   = view.findViewById(R.id.titleSection);
+        TextView bodySection    = view.findViewById(R.id.bodySection);
+        TextView detailSection  = view.findViewById(R.id.detailSection);
+
+        Typeface opensans_extrabold = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-ExtraBold.ttf");
+        Typeface opensans_bold      = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-Bold.ttf");
+        Typeface opensans_semibold  = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-SemiBold.ttf");
+        Typeface opensans_reguler   = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-Regular.ttf");
+
+        titleSection.setTypeface(opensans_bold);
+        bodySection.setTypeface(opensans_reguler);
+        detailSection.setTypeface(opensans_semibold);
+
         return view;
     }
 
