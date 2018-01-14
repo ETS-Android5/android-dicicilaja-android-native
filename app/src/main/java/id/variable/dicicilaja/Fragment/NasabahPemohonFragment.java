a57448e02fc152516eb0120b6ad8b3f52dde33de
Ziady Mubaraq <ziady.mubaraq@gmail.com>
2018-01-15 00:04:56 +0700

    DetailPengajuan on PengajuanJaminan Fragment is done

--- app/src/main/java/id/variable/dicicilaja/Fragment/NasabahPemohonFragment.java
+++ app/src/main/java/id/variable/dicicilaja/Fragment/NasabahPemohonFragment.java
@@ -6,20 +6,36 @@ import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import id.variable.dicicilaja.API.Client.ClientDetailPengajuan;
+import id.variable.dicicilaja.API.Interface.InterfaceDetailPengajuan;
+import id.variable.dicicilaja.API.Item.DetailPengajuan;
+import id.variable.dicicilaja.API.Item.DetailPengajuanResponse;
 import id.variable.dicicilaja.Activity.ProsesPengajuanActivity;
 import id.variable.dicicilaja.R;
+import id.variable.dicicilaja.Session.SessionManager;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NasabahPemohonFragment extends Fragment {
 
+    private static final String TAG = NasabahPemohonFragment.class.getSimpleName();
+    List<DetailPengajuan> detailPengajuans;
 
     public NasabahPemohonFragment() {
         // Required empty public constructor
@@ -32,13 +48,26 @@ public class NasabahPemohonFragment extends Fragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_nasabah_pemohon, container, false);
 
+        final SessionManager session = new SessionManager(getContext());
+        String apiKey = "Bearer " + session.getToken();
+
+        Bundle extras = getActivity().getIntent().getExtras();
+
         RelativeLayout proses = view.findViewById(R.id.proses);
         TextView title_nasabah = view.findViewById(R.id.title_nasabah);
         TextView title_pemohon = view.findViewById(R.id.title_pemohon);
         TextView button_telp_nasabah = view.findViewById(R.id.telephone_button_nasabah);
         TextView button_telp_pemohon = view.findViewById(R.id.telephone_button_pemohon);
-        final TextView no_telp_nasabah = view.findViewById(R.id.no_telp_nasabah);
+        final TextView no_telp_nasabah = view.findViewById(R.id.api_hp);
         final TextView no_telp_pemohon = view.findViewWithTag(R.id.no_telp_pemohon);
+
+        final TextView api_client_name = view.findViewById(R.id.api_client_name);
+        final TextView api_hp = view.findViewById(R.id.api_hp);
+        final TextView api_email = view.findViewById(R.id.api_email);
+        final TextView api_address = view.findViewById(R.id.api_address);
+        final TextView api_district = view.findViewById(R.id.api_district);
+        final TextView api_city = view.findViewById(R.id.api_city);
+
         Typeface opensans_extrabold = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-ExtraBold.ttf");
         Typeface opensans_bold = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-Bold.ttf");
         Typeface opensans_semibold = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-SemiBold.ttf");
@@ -70,6 +99,37 @@ public class NasabahPemohonFragment extends Fragment {
                 Intent call = new Intent(Intent.ACTION_DIAL);
                 call.setData(Uri.parse("tel:" +no_telp_pemohon.getText() ));
                 startActivity(call);
+            }
+        });
+
+        InterfaceDetailPengajuan apiService =
+                ClientDetailPengajuan.getClientDetailPengajuan().create(InterfaceDetailPengajuan.class);
+
+        Call<DetailPengajuanResponse> call = apiService.getDetailPengajuan(apiKey,Integer.parseInt(getActivity().getIntent().getStringExtra("EXTRA_REQUEST_ID")));
+        call.enqueue(new Callback<DetailPengajuanResponse>() {
+            @Override
+            public void onResponse(Call<DetailPengajuanResponse> call, Response<DetailPengajuanResponse> response) {
+                if ( response.isSuccessful() ) {
+                    detailPengajuans = response.body().getData();
+                    api_client_name.setText(detailPengajuans.get(0).getClientName().toString());
+                    api_hp.setText(detailPengajuans.get(0).getHp().toString());
+                    api_email.setText(detailPengajuans.get(0).getEmail().toString());
+                    api_address.setText(detailPengajuans.get(0).getAddress().toString());
+                    api_district.setText(detailPengajuans.get(0).getDistrict().toString());
+                    api_city.setText(detailPengajuans.get(0).getCity().toString());
+
+
+                } else {
+                    Toast.makeText(getContext(), "Koneksi Internet Tidak Ditemukan", Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<DetailPengajuanResponse> call, Throwable t) {
+                // Log error here since request failed
+                Toast.makeText(getContext(), "Koneksi Internet Tidak Ditemukan", Toast.LENGTH_LONG).show();
+                Log.e(TAG, t.toString());
             }
         });
         return view;
--- app/src/main/java/id/variable/dicicilaja/Fragment/PengajuanJaminanFragment.java
+++ app/src/main/java/id/variable/dicicilaja/Fragment/PengajuanJaminanFragment.java
@@ -9,9 +9,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -62,6 +65,23 @@ public class PengajuanJaminanFragment extends Fragment {
         TextView title_informasi = view.findViewById(R.id.title_informasi);
         TextView title_informasi_jaminan = view.findViewById(R.id.title_informasi_jaminan);
         final TextView api_program = view.findViewById(R.id.api_program);
+        final TextView api_product = view.findViewById(R.id.api_product);
+        final TextView api_channel = view.findViewById(R.id.api_channel);
+        final TextView api_specification = view.findViewById(R.id.api_specification);
+
+        final TextView api_colleteral = view.findViewById(R.id.api_colleteral);
+        final TextView api_manufacturer = view.findViewById(R.id.api_manufacturer);
+        final TextView api_year = view.findViewById(R.id.api_year);
+        final TextView api_tenor = view.findViewById(R.id.api_tenor);
+        final TextView api_ammount = view.findViewById(R.id.api_ammount);
+        final TextView api_area = view.findViewById(R.id.api_area);
+        final TextView api_branch = view.findViewById(R.id.api_branch);
+        final TextView api_zipcode = view.findViewById(R.id.api_zipcode);
+        final ImageView api_ktp_image = view.findViewById(R.id.api_ktp_image);
+        final ImageView api_colleteral_image = view.findViewById(R.id.api_colleteral_image);
+
+
+
         Typeface opensans_extrabold = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-ExtraBold.ttf");
         Typeface opensans_bold = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-Bold.ttf");
         Typeface opensans_semibold = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-SemiBold.ttf");
@@ -88,6 +108,21 @@ public class PengajuanJaminanFragment extends Fragment {
                 if ( response.isSuccessful() ) {
                     detailPengajuans = response.body().getData();
                     api_program.setText(detailPengajuans.get(0).getProgram().toString());
+//                    api_product.setText(detailPengajuans.get(0).getProduct().toString());
+                    api_channel.setText(detailPengajuans.get(0).getChannel().toString());
+//                    api_specification.setText(detailPengajuans.get(0).getSpecification().toString());
+                    api_colleteral.setText(detailPengajuans.get(0).getColleteral().toString());
+                    api_manufacturer.setText(detailPengajuans.get(0).getManufacturer().toString());
+                    api_year.setText(detailPengajuans.get(0).getYear().toString());
+                    api_tenor.setText(detailPengajuans.get(0).getTenor().toString() + " bulan");
+                    api_ammount.setText(detailPengajuans.get(0).getAmmount().toString());
+                    api_area.setText(detailPengajuans.get(0).getArea().toString());
+                    api_branch.setText(detailPengajuans.get(0).getBranch().toString());
+                    api_zipcode.setText("000000");
+                    String imageKtp = detailPengajuans.get(0).getKtpImage().toString();
+                    Picasso.with(getContext()).load(imageKtp).into(api_ktp_image);
+                    String imageColleteral = detailPengajuans.get(0).getColleteralImage().toString();
+                    Picasso.with(getContext()).load(imageColleteral).into(api_colleteral_image);
 
 
                 } else {
