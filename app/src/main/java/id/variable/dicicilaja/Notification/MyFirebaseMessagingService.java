4c6d4bcbb9422fd90f3c457fadcb2da0ddcd3515
Ziady Mubaraq <ziady.mubaraq@gmail.com>
2018-02-07 14:33:51 +0700

    Add Notification Downstream

--- app/src/main/java/id/variable/dicicilaja/Notification/MyFirebaseMessagingService.java
+++ app/src/main/java/id/variable/dicicilaja/Notification/MyFirebaseMessagingService.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -18,16 +19,18 @@ import com.google.firebase.messaging.RemoteMessage;
 import id.variable.dicicilaja.Activity.NotificationActivity;
 import id.variable.dicicilaja.R;
 
+import static android.content.ContentValues.TAG;
+
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
     @Override
-    public void onMessageReceived(RemoteMessage message) {
-        sendMyNotification(message.getNotification().getBody());
-    }
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
 
+        Log.d("firebase", "From: " + remoteMessage.getFrom());
+        Log.d("firebase", "Notification Message Body: " +
+                remoteMessage.getNotification().getBody());
 
-    private void sendMyNotification(String message) {
-
-        //On click of notification it redirect to this Activity
         Intent intent = new Intent(this, NotificationActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
@@ -35,8 +38,8 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Uri soundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("My Firebase Push notification")
-                .setContentText(message)
+                .setContentTitle(remoteMessage.getFrom())
+                .setContentText(remoteMessage.getNotification().getBody())
                 .setAutoCancel(true)
                 .setSound(soundUri)
                 .setContentIntent(pendingIntent);
@@ -45,5 +48,29 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         notificationManager.notify(0, notificationBuilder.build());
+
     }
+
+
+//    private void sendMyNotification(String message) {
+//
+//        //On click of notification it redirect to this Activity
+//        Intent intent = new Intent(this, NotificationActivity.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+//
+//        Uri soundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+//        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+//                .setSmallIcon(R.mipmap.ic_launcher)
+//                .setContentTitle("My Firebase Push notification")
+//                .setContentText(message)
+//                .setAutoCancel(true)
+//                .setSound(soundUri)
+//                .setContentIntent(pendingIntent);
+//
+//        NotificationManager notificationManager =
+//                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//
+//        notificationManager.notify(0, notificationBuilder.build());
+//    }
 }
