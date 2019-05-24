00f2768877fdfe8fb2f6f1b1f491e8d3b70f9b33
Ziady Mubaraq <ziady.mubaraq@gmail.com>
2019-05-24 19:49:07 +0700

    Add New Simulation Screen

---
+++ app/src/main/java/com/dicicilaja/app/Activity/NewColleteralActivity.java
@@ -0,0 +1,16 @@
+package com.dicicilaja.app.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dicicilaja.app.R;
+
+public class NewColleteralActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_colleteral);
+    }
+}
