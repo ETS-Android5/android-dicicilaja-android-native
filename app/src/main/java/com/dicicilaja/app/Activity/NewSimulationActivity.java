fdd7b13a3f8759299b7607bb11f1f1cb413b6299
Ziady Mubaraq <ziady.mubaraq@gmail.com>
2019-05-24 17:16:42 +0700

    Initial New Simulation

---
+++ app/src/main/java/com/dicicilaja/app/Activity/NewSimulationActivity.java
@@ -0,0 +1,14 @@
+package com.dicicilaja.app.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewSimulationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_simulation);
+    }
+}
