f620cd9ba335334653bcd299d8293b46035eaf22
Ziady Mubaraq <ziady.mubaraq@gmail.com>
2019-07-29 19:47:05 +0700

    change endpoint

--- app/src/main/java/com/dicicilaja/app/NewSimulation/Network/ApiClient.java
+++ app/src/main/java/com/dicicilaja/app/NewSimulation/Network/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    public static final String BASE_URL = "http://156.67.217.159:60001/api/";
+    public static final String BASE_URL = "http://103.233.103.23:81/api/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
